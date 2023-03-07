9b6c9762c17f63796c91b94dd181b58842435024
AdemYildirim <ademyildirimmakine@gmail.com>
2023-03-08 01:26:09 +0300

    Update spectrogram.py

--- spectrogram.py
+++ spectrogram.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jan 31 16:19:03 2023
 
-@author: Adem Yıldırım
+@author: u56356
 """
 
 import pandas as pd
@@ -15,15 +15,16 @@ from seffaflik.__ortak.__araclar import make_requests as __make_requests
 Baslangic_Trh = "2018-01-01"  
 Bitis_Trh = "2022-12-31"    
 
-###fiyat verisinin çekilmesi   
+###verinin çekilmesi   
 
 __first_part_url_market = "market/day-ahead-mcp"
 particular_url = __first_part_url_market + "?startDate=" + Baslangic_Trh + "&endDate=" + Bitis_Trh
 json = __make_requests(particular_url)
 ptf = pd.DataFrame(json["body"]["dayAheadMCPList"])
 ptf2 = ptf.reset_index() #saatlik veri bu tabloda
+ptf2["price1"] = ptf2["price"] + 11
+ptf2["pricelog"] = np.log10(ptf2["price1"])-np.log10(11)
 
-###tüketim verisinin çekilmesi 
 __second_part_url_market = "consumption/real-time-consumption"
 particular_url = __second_part_url_market + "?startDate=" + Baslangic_Trh + "&endDate=" + Bitis_Trh
 json = __make_requests(particular_url)
@@ -41,57 +42,63 @@ consumptions["date-mounth"]   = consumptions["date"].str[:7]
 ############Spectrogram #####################################################################
 #############################################################################################
 
-#saatlik fiyat verisinin spectrogramı
+############Saatlik Veriler #################################################################
+#saatlik veriler 
 plt.plot(ptf2["price"].values)
 plt.title('Saatlik Fiyat')  
 plt.show()
-plt.specgram(ptf2["price"].values,Fs=len(ptf2["price"])/24)
-plt.title('Saatlik Fiyat Spectrogram')  
+plt.plot(ptf2["pricelog"].values)
+plt.title('Saatlik log(Fiyat)')  
 plt.show()
-
-#günlük fiyat verisinin spectrogramı
-ptf3 = ptf2.pivot_table("price","date-daily",aggfunc="mean")
-plt.plot(ptf3["price"].values)
-plt.title('Günlük Ort Fiyat')  
-plt.show()
-plt.specgram(ptf3["price"].values,Fs=1)
-plt.title('Günlük Ort Fiyat Spectrogram')  
-plt.show()
-
-#aylık fiyat verisinin spectrogramı
-ptf4 = ptf2.pivot_table("price","date-mounth",aggfunc="mean")
-plt.plot(ptf4["price"].values)
-plt.title('Aylık Ort Fiyat')  
-plt.show()
-plt.specgram(ptf4["price"].values,Fs=1)
-plt.title('Günlük Ort Fiyat Spectrogram')  
-plt.show()
-
-#saatlik tüketim verisinin spectrogramı
 plt.plot(consumptions["consumption"].values)
 plt.title('Saatlik Tüketim')  
 plt.show()
-plt.specgram(consumptions["consumption"].values,Fs=len(consumptions["consumption"])/24)
-plt.title('Saatlik Tüketim Spectrogram')  
-plt.show()
 
-#günlük tüketim verisinin spectrogramı
+#saatlik spectrogramlar
+fig = plt.figure()
+gs = fig.add_gridspec(1, 2, hspace=0, wspace=0)
+(ax1, ax2) = gs.subplots(sharex='col', sharey='row')
+fig.suptitle('Saatlik Spectrogramlar \n(fiyat,tüketim)')
+ax1.specgram(ptf2["pricelog"].values,Fs=len(ptf2["pricelog"])/24)
+ax2.specgram(consumptions["consumption"].values,Fs=len(consumptions["consumption"])/24)
+
+############Günlük Veriler ##################################################################
+#günlük veriler
+ptf3 = ptf2.pivot_table("pricelog","date-daily",aggfunc="mean")
+plt.plot(ptf3["pricelog"].values)
+plt.title('Günlük Ort Fiyat')  
+plt.show()
 consumptions2 = consumptions.pivot_table("consumption","date-daily",aggfunc="mean")
 plt.plot(consumptions2["consumption"].values)
 plt.title('Günlük Ort Tüketim')  
 plt.show()
-plt.specgram(consumptions2["consumption"].values,Fs=1)
-plt.title('Günlük Ort Tüketim Spectrogram')  
-plt.show()
 
-#aylık tüketim verisinin spectrogramı
+#günlük spectrogramlar
+fig = plt.figure()
+gs = fig.add_gridspec(1, 2, hspace=0, wspace=0)
+(ax1, ax2) = gs.subplots(sharex='col', sharey='row')
+fig.suptitle('Günlük Spectrogramlar \n(fiyat,tüketim)')
+ax1.specgram(ptf3["pricelog"].values,Fs=1)
+ax2.specgram(consumptions2["consumption"].values,Fs=1)
+
+############Aylık Veriler ###################################################################
+#Aylık Veriler
+ptf4 = ptf2.pivot_table("pricelog","date-mounth",aggfunc="mean")
+plt.plot(ptf4["pricelog"].values)
+plt.title('Aylık Ort Fiyat')  
+plt.show()
 consumptions3 = consumptions.pivot_table("consumption","date-mounth",aggfunc="mean")
 plt.plot(consumptions3["consumption"].values)
 plt.title('Aylık Ort Tüketim')  
 plt.show()
-plt.specgram(consumptions3["consumption"].values,Fs=1)
-plt.title('Aylık Ort Tüketim Spectrogram')  
-plt.show()
+
+#aylık spectrogramlar
+fig = plt.figure()
+gs = fig.add_gridspec(1, 2, hspace=0, wspace=0)
+(ax1, ax2) = gs.subplots(sharex='col', sharey='row')
+fig.suptitle('Aylık Spectrogramlar \n(fiyat,tüketim)')
+ax1.specgram(ptf4["pricelog"].values,Fs=1)
+ax2.specgram(consumptions3["consumption"].values,Fs=1)
 
 
 #############################################################################################
@@ -103,7 +110,7 @@ plt.show()
 
 
 from pandas.plotting import autocorrelation_plot
-autocorrelation_plot(ptf2["price"])
+autocorrelation_plot(ptf2["pricelog"])
 plt.show()
 
 from statsmodels.graphics.tsaplots import plot_acf
@@ -111,19 +118,19 @@ from statsmodels.graphics.tsaplots import plot_pacf
 import statsmodels as sm
 
 ###################### acf ler 
-sm.graphics.tsaplots.plot_acf(ptf2["price"],lags=7*24)
+sm.graphics.tsaplots.plot_acf(ptf2["pricelog"],lags=7*24)
 plt.title('saatlik fiyat acf')  
 plt.show()
 sm.graphics.tsaplots.plot_acf(consumptions["consumption"],lags=24*7)
 plt.title('saatlik tüketim acf')  
 plt.show()
-sm.graphics.tsaplots.plot_acf(ptf3["price"],lags=7)
+sm.graphics.tsaplots.plot_acf(ptf3["pricelog"],lags=7)
 plt.title('günlük fiyat acf')  
 plt.show()
 sm.graphics.tsaplots.plot_acf(consumptions2["consumption"],lags=100)
 plt.title('günlük tüketim acf')  
 plt.show()
-sm.graphics.tsaplots.plot_acf(ptf4["price"],lags=12)
+sm.graphics.tsaplots.plot_acf(ptf4["pricelog"],lags=12)
 plt.title('aylık fiyat acf')  
 plt.show()
 sm.graphics.tsaplots.plot_acf(consumptions3["consumption"],lags=12)
@@ -131,19 +138,19 @@ plt.title('aylık tüketim acf')
 plt.show()
 
 ###################### pacf ler 
-sm.graphics.tsaplots.plot_pacf(ptf2["price"],lags=7*24)
+sm.graphics.tsaplots.plot_pacf(ptf2["pricelog"],lags=7*24)
 plt.title('saatlik fiyat pacf')  
 plt.show()
 sm.graphics.tsaplots.plot_pacf(consumptions["consumption"],lags=24*7)
 plt.title('saatlik tüketim pacf')  
 plt.show()
-sm.graphics.tsaplots.plot_pacf(ptf3["price"],lags=7)
+sm.graphics.tsaplots.plot_pacf(ptf3["pricelog"],lags=7)
 plt.title('günlük fiyat pacf')  
 plt.show()
 sm.graphics.tsaplots.plot_pacf(consumptions2["consumption"],lags=100)
 plt.title('günlük tüketim pacf')  
 plt.show()
-sm.graphics.tsaplots.plot_pacf(ptf4["price"],lags=12)
+sm.graphics.tsaplots.plot_pacf(ptf4["pricelog"],lags=12)
 plt.title('aylık fiyat pacf')  
 plt.show()
 sm.graphics.tsaplots.plot_pacf(consumptions3["consumption"],lags=12)
